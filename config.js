90cbe844616f7f36fa5eb27986a4eadc68180975
hyou59720-ctrl <hyou59720@gmail.com>
2026-08-29 19:37:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = "https://compulsory-marmoset-mkalayu-36a5547d.koyeb.app/";
-const API_BASE_URL2 = "https://webapp-lpjw.onrender.com/";
+const API_BASE_URL2 = "https://compulsory-marmoset-mkalayu-36a5547d.koyeb.app/";
+const API_BASE_URL = "https://webapp-lpjw.onrender.com/";
